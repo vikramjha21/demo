c5faab8aed8ae89c103d169a6a2b2199712732f4
vikramjha21 <vkumar210795@gmail.com>
2022-07-04 20:34:16 +0530

    modified ABC.java

--- ABC.java
+++ ABC.java
@@ -1,5 +1,6 @@
 public ABC{
    public static void main(String args[]){
-    System.out.println("From ABC class!!");
+for(int i=0;i<n;i++){
+    System.out.println("From ABC class for loop!!");}
    }
 }
